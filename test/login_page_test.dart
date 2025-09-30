a867a95cd7253b2ebcc79fb93e2220fe44375dce
NKTKLN <nktkln@nktkln.com>
2025-09-30 08:02:49 +0300

    Minor update

--- test/login_page_test.dart
+++ test/login_page_test.dart
@@ -1,5 +1,4 @@
 import 'package:build_inspect/pages/login_page.dart';
-import 'package:build_inspect/pages/projects_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
